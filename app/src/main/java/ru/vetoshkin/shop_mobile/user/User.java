db453f621a8a82e0669866ac63d0575946eca9b7
topkdr4 <topkdr4@mail.ru>
2018-09-22 13:27:47 +0500

    login layout

--- app/src/main/java/ru/vetoshkin/shop_mobile/user/User.java
+++ app/src/main/java/ru/vetoshkin/shop_mobile/user/User.java
@@ -1,11 +1,15 @@
 package ru.vetoshkin.shop_mobile.user;
 
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@RequiredArgsConstructor
 public class User {
-    protected String login;
-    protected String password;
+    protected final String login;
+    protected final String password;
     @Setter
     protected String sessionId;
 }
