1cea9a0f52795581246da78feb372ffd18ce63d8
topkdr4 <topkdr4@mail.ru>
2019-03-10 01:18:04 +0500

    splash screen

--- app/src/main/java/ru/vetoshkin/shop_mobile/MainLoader.java
+++ app/src/main/java/ru/vetoshkin/shop_mobile/MainLoader.java
@@ -4,43 +4,63 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.AndroidRuntimeException;
-import pl.droidsonroids.gif.GifDrawable;
-import pl.droidsonroids.gif.GifImageView;
-import ru.vetoshkin.shop_mobile.config.AppConfig;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.vetoshkin.shop_mobile.config.AppConfig;
 
 
 
 
 
 public class MainLoader extends Activity {
-    private static final String propertyFile = "app.properties";
+    private final AlphaAnimation animation = createAnimation();
+
+    @BindView(R.id.screen_logo)
+    ImageView screen;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try (InputStream stream = getBaseContext().getAssets().open(propertyFile)) {
-            Properties props = new Properties();
-            props.load(stream);
-            AppConfig.init(props);
-        } catch (IOException e) {
-            throw new AndroidRuntimeException(e);
-        }
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main_loader);
-
-        GifImageView preloadView = findViewById(R.id.preload_gif);
-        GifDrawable drawable = (GifDrawable) preloadView.getDrawable();
-        drawable.addAnimationListener(loopNumber -> {
-            Intent intent = new Intent(MainLoader.this, MainActivity.class);
-            startActivity(intent);
-        });
+        ButterKnife.bind(this);
+        screen.setAnimation(animation);
     }
+
+
+    private AlphaAnimation createAnimation() {
+        AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f) ;
+        animation.setFillAfter(true);
+        animation.setDuration(1000);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(MainLoader.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        return animation;
+    }
+
 
 
 }
